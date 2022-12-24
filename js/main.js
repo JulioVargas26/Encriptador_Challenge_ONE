a7859f577f1752ca10e7126ef88234cac2e20612
JulioVargas26 <84108359+JulioVargas26@users.noreply.github.com>
2022-12-24 17:24:29 -0500

    24/12/22

--- js/main.js
+++ js/main.js
@@ -9,67 +9,36 @@ function limpiar() {
 }
 
 function encriptar() {
-    let nuevoTexto = document.getElementById("texto_area").value;
-    nuevoTexto;
+    var nuevoTexto = document.getElementById("texto_area").value;
     nuevoTexto = nuevoTexto
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
-    nuevoTexto;
     document.getElementById("texto_resultado").value = nuevoTexto;
 }
 
 function desencriptar() {
-    let nuevoTexto = document.getElementById("texto_area").value;
-    nuevoTexto;
+    var nuevoTexto = document.getElementById("texto_area").value;
     nuevoTexto = nuevoTexto
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
-
-    nuevoTexto;
     document.getElementById("texto_resultado").value = nuevoTexto;
 }
 
 function copiar() {
     document.getElementById("texto_area").placeholder = "";
-    let textCopi = document.getElementById("texto_resultado");
-    textCopi.select();
+    var textCopia = document.getElementById("texto_resultado");
+    textCopia.select();
     document.execCommand("copy");
-    document.getElementById("texto_area").value = "";
-    document.getElementById("texto_resultado").value = "";
+    limpiar();
 
 }
 
-function validar() {
-    let nuevomensaje = document.getElementById("texto_area").value;
-    if (nuevomensaje.match(letras) != null) {
-        limpiar();
-        Swal.fire({
-            position: 'center',
-            icon: 'error',
-            title: 'Solo letras minúsculas y sin acentos',
-            showConfirmButton: true,
-            background: '#f8ed8f',
-            backdrop: `
-            rgba(123, 8, 0, 0.4)
-            url("./img/nyan-cat.gif")
-            left top
-            no-repeat
-          `
-           
-          })
-    }
-}
-
-botonE.addEventListener("click", validar);
 botonE.addEventListener("click", encriptar);
-
-botonD.addEventListener("click", validar);
 botonD.addEventListener("click", desencriptar);
-
 botonC.addEventListener("click", copiar);
